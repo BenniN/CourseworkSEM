a38b5047d543ac5076b0f10d5ab60fe3fdc5d7eb
Domenic Cassisi <domenic.cassisi@web.de>
2020-01-22 15:54:08 +0000

    Created simple Hello World app for further testing

---
+++ src/main/java/com/napier/sem/Main.java
@@ -0,0 +1,13 @@
+package com.napier.sem;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO, "Application started...");
+    }
+}
